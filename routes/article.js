f5d8f67afa1f1749e74955221cd430c8791f57c8
neoyyyy <neoyyyy@neoyyyydeMacBook-Pro.local>
2017-12-10 17:19:58 +0800

    添加用户分享api

--- routes/article.js
+++ routes/article.js
@@ -4,14 +4,14 @@ var logger = require('../frame/log/logger');
 var articleservice = require('../service/articleservice');
 
 
-router.route("/getarticles")
+router.route("/article/getarticles")
     .post(function (req,res) {
         logger.info('getarticles post data:'+JSON.stringify(req.body));
         articleservice.getArticleList(req,res);
     });
 
 
-router.route("/createarticle")
+router.route("/article/createarticle")
     .post(function (req,res) {
         logger.info('create article post data:'+JSON.stringify(req.body));
         articleservice.createArticle(req,function (err) {
@@ -19,15 +19,29 @@ router.route("/createarticle")
         });
     });
 
-router.route('/update')
+router.route('/article/update/:articleid')
     .post(function (req,res) {
         logger.info('update article id:'+JSON.parse(JSON.stringify(req.body)).articleid);
         articleservice.updateArticle(req,function (callback) {
 
         })
 
-    })
+    });
 
+router.route('/article/share/:articleid')
+    .get(function (req,res) {
+
+    });
+
+router.route('/article/getsharearticle/:articleid')
+    .get(function (req,res) {
+
+    });
+
+router.route('article/delete/:articleid')
+    .get(function (req,res) {
+
+    });
 
 
 
